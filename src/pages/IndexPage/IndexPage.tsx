9d83f200fee2794bbb191709bde4bc103d6f0f50
vwsode <frscode1@gmail.com>
2024-08-28 19:12:24 +0500

    fix: build config

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -4,7 +4,6 @@ import type { FC } from "react";
 import { HoroscopeList } from "@/components/HoroscopeList/HoroscopeList";
 import { HoroscopeModal } from "@/components/HoroscopeModal/HoroscopeModal";
 import { useHoroscopeModal } from "@/contexts/HoroscopeModalContext";
-import React from "react";
 
 export const IndexPage: FC = () => {
   const { isOpen, title, description } = useHoroscopeModal();
@@ -14,7 +13,6 @@ export const IndexPage: FC = () => {
     <List>
       {/* <button onClick={() => setOpen((prev) => !prev)}>Opne</button> */}
       <HoroscopeList />
-
 
       <HoroscopeModal isOpen={isOpen} description={description} title={title} />
     </List>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
